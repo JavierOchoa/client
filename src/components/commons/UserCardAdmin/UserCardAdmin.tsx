94771b6cd0952061d068eb9344f5a9c3c36c77d5
Fran Mansilla <75451439+FranciscoMansilla@users.noreply.github.com>
2022-06-12 13:37:53 -0300

    pruebas con libreria Swal

--- src/components/commons/UserCardAdmin/UserCardAdmin.tsx
+++ src/components/commons/UserCardAdmin/UserCardAdmin.tsx
@@ -1,4 +1,5 @@
 import m from './UserCardAdmin.module.css'
+import Swal from 'sweetalert2'
 
 interface user{
   id:string,
@@ -9,7 +10,28 @@ interface user{
 }
 
 const UserCardAdmin = (user:user)=>{
+  const clickEdit= ()=>{
 
+  }
+  const clickDelete= ()=>{
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        )
+      }
+    })
+  }
   return(
     <div className={m.container}>
       <div className={m.infoContainer}>
@@ -28,11 +50,8 @@ const UserCardAdmin = (user:user)=>{
           <div className={m.divSong}>
             <span className={m.span}>Songs number: {user.songNumber}</span>
           </div>
-          <button className={m.button}><img className={m.img} src="https://cdn-icons-png.flaticon.com/512/1827/1827933.png" alt="edit" /></button>
-          <button className={m.button}><img className={m.img} src="https://cdn-icons.flaticon.com/png/512/542/premium/542724.png?token=exp=1654784155~hmac=6cbdff0d6916b7b638a797e3a99baeae" alt="delete" /></button>
-
-
-
+          <button onClick={clickEdit} className={m.button}><img className={m.img} src="https://cdn-icons-png.flaticon.com/512/1827/1827933.png" alt="edit" /></button>
+          <button onClick={clickDelete} className={m.button}><img className={m.img} src="https://cdn-icons.flaticon.com/png/512/542/premium/542724.png?token=exp=1654784155~hmac=6cbdff0d6916b7b638a797e3a99baeae" alt="delete" /></button>
 
       </div>
     </div>
